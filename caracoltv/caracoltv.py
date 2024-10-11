ec29c80ad6554b6cb73f00f0bd668275a5c4ac1b
Leo <leocasti2@gmail.com>
2024-10-11 03:39:42 -0500

    feat: agrega capacidad para detectar las URL compatibles y organiza parte del código

--- caracoltv/caracoltv.py
+++ caracoltv/caracoltv.py
@@ -5,79 +5,27 @@ from . import caracoltv_utils as utils
 from urllib.parse import urljoin, urlparse
 
 class CaracolTv:
-
-    def __init__(self):
-        self._index = 1
-
-    @property
-    def index(self):
-        return getattr(self, "_index")
-
-    def _make_request(self, url, method_head=False):
-        headers = {
-            "sec-ch-ua": '"Chromium";v="118", "Brave";v="118", "Not=A?Brand";v="99"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-            "Sec-GPC": "1",
-            "Sec-Fetch-Site": "same-origin",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-User": "?1",
-            "Sec-Fetch-Dest": "document",
-        }
-        if method_head is True:
-            return requests.head(
-                url=url,
-                headers=headers,
-            )
-
-        return requests.get(url=url, headers=headers)
-    
-    def _get_pagination_url(self,url):
-        """
-        Obtiene la URL base de la paginación, eliminando el índice de la página actual.
-
-        Si la URL proporcionada ya contiene un query de paginación (ej. `...page=16`), se devolverá la URL con el query de paginación sin el índice,
-        dejando el formato como `...page=`.
-
-        Si la URL no contiene un query de paginación, se hace una solicitud a la página para obtener la URL de la siguiente página, y se formatea de la misma manera eliminando el índice de la paginación.
-
-        Args:
-            url (str): La URL de la cual se quiere obtener la base de la paginación.
-
-        Returns:
-            str: La URL con el query de paginación sin el índice.
-        """
-
-        if utils.has_pagination_query(url):
-            query = utils.get_query_pagination_without_index(url)
-            return urlparse(url)._replace(query=query).geturl()
-        else:
-            response = self._make_request(url)
-            root = etree.fromstring(response.text, etree.HTMLParser())
-            next_page_url = utils.get_next_page_url(root)
-            query = utils.get_query_pagination_without_index(next_page_url)
-            return urlparse(url)._replace(query=query).geturl()
-    
     def get_articles(self, url: str, start_index: int = 1) -> Iterable[list[dict]]:
         index = 1
-        last_box = False
-        pagination_url = self._get_pagination_url(url)
-
+        root = utils.get_root(url)
+        section= root.find(".//li[@data-current-nav-item]//a").text
+        
+        if utils.support_section(section) is False:
+            raise ValueError(f"La URL no es compatible:  {url}")
+             
+        pagination_url = utils.get_pagination_from_url_or_root(url, root)
+        next_page_url= None
+        
         if start_index != 1:
             index = start_index
             url = pagination_url + str(index)
             next_page_url = pagination_url + str(index + 1)
             last_box = True
 
-        while True:
-            response = self._make_request(url)
-            root = etree.fromstring(response.text, etree.HTMLParser())
-            articles = utils.extract_articles(root, last_box=last_box)
-
-            next_page_url = pagination_url + str(index + 1)
+        while True:            
+            articles = utils.extract_articles(root, index=index)
+            next_page_url = pagination_url + str(index + 1) if pagination_url else None
+            
             if len(articles) == 0:
                 break
             
@@ -86,8 +34,11 @@ class CaracolTv:
                 "index": index,
                 "articles": articles,
                 "next_page_url": next_page_url,
+                "section": section
             }
-
-            url = next_page_url
+            
             index += 1
-            last_box = True
+            if next_page_url:
+                root = utils.get_root(next_page_url)
+            else:
+                break
--- caracoltv/caracoltv_utils.py
+++ caracoltv/caracoltv_utils.py
@@ -1,9 +1,10 @@
-
-from datetime import datetime
 from urllib.parse import urljoin, urlparse
+import requests
+from lxml import etree
 
-def extract_articles(root, last_box=False):
-    target_class= "" if not last_box else "//section[@class='TwoColumnContainer3070']"
+
+def extract_articles(root, index=1):
+    target_class= "" if index <= 1 else "//section[@class='TwoColumnContainer3070']"
 
     articles = []
     for element in root.xpath(f".{target_class}//*[@class='PromoB-content']"):
@@ -25,7 +26,6 @@ def extract_articles(root, last_box=False):
         
     return articles
 
-
 def get_next_page_url(root):
     """
     Obtiene la URL de la próxima página de paginación a partir de un documento HTML.
@@ -39,8 +39,10 @@ def get_next_page_url(root):
     next_page_element = root.find(
         ".//*[@class='TwoColumnContainer3070']//a[@title='CARGAR MÁS']"
     )
-    next_page_url = urljoin(url, next_page_element.get("data-original-href"))
-    return next_page_url
+    if next_page_element is not None:
+        next_page_url = urljoin(url, next_page_element.get("data-original-href"))
+        return next_page_url
+    return None
 
 def has_pagination_query(url):
     """
@@ -76,4 +78,74 @@ def get_query_pagination_without_index(url):
     urlparsed = urlparse(url)
     return urlparsed.query.split("=")[0] + "="
 
+def get_pagination_from_url(url):
+    """
+    Obtiene la URL base de la paginación de la url, eliminando el índice de la página actual.
 
+    Si la URL proporcionada ya contiene un query de paginación (ej. `...page=16`), se devolverá la URL con el query de paginación sin el índice,
+    dejando el formato como `...page=`.
+    
+    Args:
+        url (str): La URL de la cual se quiere obtener la base de la paginación.
+
+    Returns:
+        str: La URL con el query de paginación sin el índice.
+    """
+    query = get_query_pagination_without_index(url)
+    return urlparse(url)._replace(query=query).geturl()
+
+def get_pagination_from_root(root=None):
+    """
+    Obtiene la URL base de la paginación del root, eliminando el índice de la página actual.
+
+    Returns:
+        str: La URL con el query de paginación sin el índice.
+    """
+    url= root.find(".//meta[@property='og:url']").get("content")
+    next_page_url = get_next_page_url(root)
+    if next_page_url:
+        query = get_query_pagination_without_index(next_page_url)
+        return urlparse(url)._replace(query=query).geturl()
+
+def make_request(url, method_head=False):
+    headers = {
+        "sec-ch-ua": '"Chromium";v="118", "Brave";v="118", "Not=A?Brand";v="99"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "Sec-GPC": "1",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-User": "?1",
+        "Sec-Fetch-Dest": "document",
+    }
+    if method_head is True:
+        return requests.head(
+            url=url,
+            headers=headers,
+        )
+
+    return requests.get(url=url, headers=headers)
+
+def get_root(url):
+    response = make_request(url)
+    root = etree.fromstring(response.content, etree.HTMLParser())
+    response.raise_for_status()        
+    return root
+
+def get_pagination_from_url_or_root(url, root):
+    if has_pagination_query(url):
+        return get_pagination_from_url(url)
+    else:
+        return get_pagination_from_root(root)
+
+def support_section(section):
+    # Verifica si la sección es de tipo capítulo o inicio
+    if "capítulo" in section.lower():
+        return True
+    elif "inicio" in section.lower():
+        return True
+    else:
+        return False
