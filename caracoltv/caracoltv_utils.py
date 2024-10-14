9c4c11d2329d826ce73e5187fd5394ea39009fd4
Leo <leocasti2@gmail.com>
2024-10-14 02:19:17 -0500

    feat: añade método get_page para extraer datos clave y contenido HTML de páginas de CaracolTV, y agrega archivo setup.py al proyecto

--- caracoltv/caracoltv_utils.py
+++ caracoltv/caracoltv_utils.py
@@ -1,32 +1,43 @@
 from urllib.parse import urljoin, urlparse
 import requests
 from lxml import etree
+import json
 
 
 def extract_articles(root, index=1):
-    target_class= "" if index <= 1 else "//section[@class='TwoColumnContainer3070']"
+    target_class = "" if index <= 1 else "//section[@class='TwoColumnContainer3070']"
 
     articles = []
     for element in root.xpath(f".{target_class}//*[@class='PromoB-content']"):
         string_timestamp = element.find(".//div[@data-date]").get("data-timestamp")
 
-        title= element.find(".//h2[@class='PromoB-title']//a").text.strip()
-        description= element.find(".//h3[@class='PromoB-description']//a").text.strip()
-        category= element.find(".//div[@class='PromoB-title-touch']//svg/use").get("xlink:href")
-        thumbnail= element.find(".//div[@class='PromoB-media']//img").get("srcset").split(",")[-1].split()[0]
-        url= element.find(".//h2[@class='PromoB-title']//a").get("href")
-        timestamp= int(string_timestamp)
+        title = element.find(".//h2[@class='PromoB-title']//a").text.strip()
+        description = element.find(".//h3[@class='PromoB-description']//a").text.strip()
+        category = element.find(".//div[@class='PromoB-title-touch']//svg/use").get(
+            "xlink:href"
+        )
+        thumbnail = (
+            element.find(".//div[@class='PromoB-media']//img")
+            .get("srcset")
+            .split(",")[-1]
+            .split()[0]
+        )
+        url = element.find(".//h2[@class='PromoB-title']//a").get("href")
+        timestamp = int(string_timestamp)
 
-        articles.append({
-            "title": title,
-            "description": description,
-            "category": category,
-            "thumbnail": thumbnail,
-            "timestamp": timestamp,
-            "url": url
-            })
-        
+        articles.append(
+            {
+                "title": title,
+                "description": description,
+                "category": category,
+                "thumbnail": thumbnail,
+                "timestamp": timestamp,
+                "url": url,
+            }
+        )
+
     return articles
+
 
 def get_next_page_url(root):
     """
@@ -46,6 +57,7 @@ def get_next_page_url(root):
         return next_page_url
     return None
 
+
 def has_pagination_query(url):
     """
     Verifica si una URL contiene un query de paginación.
@@ -61,6 +73,7 @@ def has_pagination_query(url):
     # Verifica si la url tiene el query de la paginacion
     urlparsed = urlparse(url)
     return "page=" in urlparsed.query
+
 
 def get_query_pagination_without_index(url):
     """
@@ -80,13 +93,14 @@ def get_query_pagination_without_index(url):
     urlparsed = urlparse(url)
     return urlparsed.query.split("=")[0] + "="
 
+
 def get_pagination_from_url(url):
     """
     Obtiene la URL base de la paginación de la url, eliminando el índice de la página actual.
 
     Si la URL proporcionada ya contiene un query de paginación (ej. `...page=16`), se devolverá la URL con el query de paginación sin el índice,
     dejando el formato como `...page=`.
-    
+
     Args:
         url (str): La URL de la cual se quiere obtener la base de la paginación.
 
@@ -96,6 +110,7 @@ def get_pagination_from_url(url):
     query = get_query_pagination_without_index(url)
     return urlparse(url)._replace(query=query).geturl()
 
+
 def get_pagination_from_root(root=None):
     """
     Obtiene la URL base de la paginación del root, eliminando el índice de la página actual.
@@ -103,11 +118,12 @@ def get_pagination_from_root(root=None):
     Returns:
         str: La URL con el query de paginación sin el índice.
     """
-    url= root.find(".//meta[@property='og:url']").get("content")
+    url = root.find(".//meta[@property='og:url']").get("content")
     next_page_url = get_next_page_url(root)
     if next_page_url:
         query = get_query_pagination_without_index(next_page_url)
         return urlparse(url)._replace(query=query).geturl()
+
 
 def make_request(url, method_head=False):
     headers = {
@@ -131,17 +147,20 @@ def make_request(url, method_head=False):
 
     return requests.get(url=url, headers=headers)
 
+
 def get_root(url):
     response = make_request(url)
     root = etree.fromstring(response.content, etree.HTMLParser())
-    response.raise_for_status()        
+    response.raise_for_status()
     return root
+
 
 def get_pagination_from_url_or_root(url, root):
     if has_pagination_query(url):
         return get_pagination_from_url(url)
     else:
         return get_pagination_from_root(root)
+
 
 def support_section(section):
     # Verifica si la sección es de tipo capítulo o inicio
@@ -151,3 +170,56 @@ def support_section(section):
         return True
     else:
         return False
+
+
+def extract_multimedia_data(root):
+    """
+    Extrae las URLs de los videos e imagenes incrustados desde varias fuentes en una página HTML.
+
+    Esta función busca videos de YouTube, MediaStream e imagenes en carruseles (CarouselSlide) en el árbol HTML proporcionado y organiza sus URLs y metadatos en un diccionario categorizado.
+
+    Args:
+        root: Un objeto que representa el árbol del documento HTML.
+
+    Returns:
+        dict: Un diccionario con listas de URLs de videos y diapositivas. Las claves del diccionario son:
+            - "youtube": Lista de URLs de videos de YouTube.
+            - "mediastream": Lista de diccionarios con información de videos MediaStream, incluyendo el título del video, la URL de la playlist y las miniaturas.
+            - "carousel": Lista de URLs de imágenes de carruseles (diapositivas).
+    """
+    videos = {"youtube": [], "mediastream": [], "carousel": []}
+
+    # Youtube
+    elements = root.xpath(".//iframe[@class='YouTubeExternalContentUrl-iframe']")
+    for element in elements:
+        url = element.get("src")
+        videos["youtube"].append(url)
+
+    # MediaStream HSL
+    elements = root.xpath(".//ps-mediastream[@class='MediaStreamVideoPlayer']")
+    for element in elements:
+        data_mediastream = element.find(".//*[@data-mediastream]").get(
+            "data-mediastream"
+        )
+        data = json.loads(data_mediastream)[0]
+        video_title = element.get("data-video-title").strip()
+        playlist_url = f"https://mdstrm.com/embed/{data['videoId']}"
+        thumbnails = [
+            value.split()[0]
+            for value in element.find(".//img").get("srcset").split(",")
+        ]
+        videos["mediastream"].append(
+            {
+                "video_title": video_title,
+                "playlist_url": playlist_url,
+                "thumbnails": thumbnails,
+            }
+        )
+
+    # CarouselSlide
+    elements = root.xpath(".//div[@class='CarouselSlide-media']")
+    for element in elements:
+        url = element.find(".//img").get("src")
+        videos["carousel"].append(url)
+
+    return videos
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pycaracoltv",
+    version="0.3",
+    description="",
+    author="Leo",
+    author_email="leocasti@gmail.com",
+    packages=find_packages(),
+    install_requires=["lxml", "requests"],
+)
